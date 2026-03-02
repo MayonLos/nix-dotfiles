113ff10c5c6fef90c0411f625aaac0a79fed72e2
MayonLos <ml20061023@outlook.com>
2026-03-02 08:37:26 +0800

    fix audio

--- system/audio.nix
+++ system/audio.nix
@@ -12,6 +12,40 @@
     pulse.enable = true;
     wireplumber.enable = true;
     jack.enable = true;
+    extraConfig = {
+      pipewire."90-echo-cancel" = {
+        "context.modules" = [
+          {
+            name = "libpipewire-module-echo-cancel";
+            args = {
+              "library.name" = "aec/libspa-aec-webrtc";
+              "source.props" = {
+                "node.name" = "echo_cancelled_source";
+                "node.description" = "Echo Cancelled Source";
+              };
+              "sink.props" = {
+                "node.name" = "echo_cancelled_sink";
+                "node.description" = "Echo Cancelled Sink";
+              };
+            };
+          }
+        ];
+      };
+
+      pipewire-pulse."91-qq-wechat-compat" = {
+        "pulse.rules" = [
+          {
+            matches = [
+              { "application.process.binary" = "qq"; }
+              { "application.process.binary" = "~wechat.*"; }
+            ];
+            actions = {
+              quirks = [ "force-s16-info" ];
+            };
+          }
+        ];
+      };
+    };
   };
 
   security.rtkit.enable = true;
