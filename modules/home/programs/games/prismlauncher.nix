ef86387cd0a7093fad112ff5163d2f412e94238e
MayonLos <ml20061023@outlook.com>
2026-05-08 19:29:58 +0800

    fix some problems

--- modules/home/programs/games/prismlauncher.nix
+++ modules/home/programs/games/prismlauncher.nix
@@ -5,10 +5,6 @@ let
     jdks = [ pkgs.javaPackages.compiler.temurin-bin.jdk-25 ];
     gamemodeSupport = true;
   };
-  # With NVIDIA PRIME offload configured, libglvnd loads NVIDIA EGL first
-  # (10_nvidia.json < 50_mesa.json). NVIDIA EGL can't serve a Wayland
-  # display in PRIME offload mode and crashes GLFW's EGL context creation.
-  # Pin to Mesa EGL so glfw3-minecraft can open a native Wayland window.
   prismWrapped = pkgs.symlinkJoin {
     name = "prismlauncher";
     paths = [ prismBase ];
@@ -16,7 +12,9 @@ let
     postBuild = ''
       wrapProgram $out/bin/prismlauncher \
         --set __EGL_VENDOR_LIBRARY_FILENAMES \
-          /run/opengl-driver/share/glvnd/egl_vendor.d/50_mesa.json
+          /run/opengl-driver/share/glvnd/egl_vendor.d/50_mesa.json \
+        --set ZINK_PERF_RELAX 1 \
+        --set MESA_NO_ERROR 1
     '';
   };
 in
